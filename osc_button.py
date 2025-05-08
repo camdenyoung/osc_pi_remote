cb01bfd455b2407f82130d41ce329fd20e5df402
camden <camdenyoung@me.com>
2025-05-08 17:04:53 +1000

    Added user input for testing

--- osc_button.py
+++ osc_button.py
@@ -9,9 +9,12 @@ from signal import pause
 
 # OSC Setup
 osc_dest_ip = "10.27.5.189"     # Qlab Destination IP
-osc_dest_port = 53000           # Qlab Listen Port
+osc_dest_port = 53000
+osc_listen_ip = "0.0.0.0"           # Qlab Listen Port
 osc_listen_port = 53001         # RPi Listen Port
 client = SimpleUDPClient(osc_dest_ip, osc_dest_port)
+
+number = 0
 
 osc_update_path = "/updates"
 osc_update_state = 1
@@ -21,27 +24,28 @@ button1_on_value = 1
 button1_off_path = "path/to/qlab"
 button1_off_value = 0
 
-button2_on_path = "path/to/qlab"
+button2_on_path = "/cue/3.5/pause"
 button2_on_value = 0
 button2_off_path = "path/to/qlab"
 button2_off_value = 0
 
-button3_on_path = "path/to/qlab"
+button3_on_path = "/cue/3.5/stop"
 button3_on_value = 0
 button3_off_path = "path/to/qlab"
 button1_off_value = 0
 
-button4_on_path = "path/to/qlab"
+button4_on_path = "/cue/3.5/load"
 button4_on_value = 0
 button4_off_path = "path/to/qlab"
 button4_off_value = 0
+
 
 def receive_updates(path, value):
     print("Subcribing to Updates")
     client.send_message(path, value)
 
 def button_pressed(path, value):
-    print("Button pressed! Cue 3.5 Triggered")
+    print("Button pressed! Cue", {number}, "Triggered")
     client.send_message(path, value)  # Replace with your OSC path/message
 
 
@@ -50,4 +54,23 @@ def button_pressed(path, value):
 #receive_updates("/alwaysReply", 0)
 
 # Test to send osc message immediately with executed script
-button_pressed(button1_on_path, 1)
+# button_pressed(button1_on_path, 1)
+
+while True:
+        try:
+            user_input = input("Enter a number: ")
+            number = int(user_input)
+            print("You entered:", number)
+            if number == 1:
+                button_pressed(button1_on_path, 1)
+            elif number == 2:
+                button_pressed(button2_on_path, 1)
+            elif number == 3:
+                button_pressed(button3_on_path, 1)
+            elif number == 4:
+                button_pressed(button4_on_path, 1)
+            else:
+                print("Not valid input")
+
+        except ValueError:
+             print("Invalid Input")
